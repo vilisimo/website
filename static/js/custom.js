54a08ad1396a1ac23d33866b0e52efb9a4543430
Simonas Viliunas <vilisimo@gmail.com>
2018-01-19 20:39:44 +0200

    Add toggle to project description

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,3 @@
-var elements = document.getElementsByClassName('project-content');
-Array.from(elements).forEach(element => element.addEventListener('click', function(e) {
-    e = e || window.event;
-    var target = e.target || e.srcElement;
-    var text = target.textContent || text.innerText;
-    target.style.display = "none";  
-}, false));
+$('.project-title').click(function() {
+	$(this).next('div').toggle(200);
+});
