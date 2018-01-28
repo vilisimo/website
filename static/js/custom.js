2369d96a7f4a757bd8e50cc3c19cbfee287e3474
Simonas Viliunas <vilisimo@gmail.com>
2018-01-28 14:19:40 +0200

    Adjust expansion of project content to toggle only on title

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,5 @@
-$('.project').click(function() {
-  $(this).children('.project-content').toggle(300).removeClass('hidden');
-  $(this).siblings().children('.project-content').hide(300).addClass('hidden');
+$('.project-title').click(function() {
+    const project = $(this).closest('.project');
+    project.children('.project-content').toggle(300).removeClass('hidden');
+    project.siblings().children('.project-content').hide(300).addClass('hidden');
 });
