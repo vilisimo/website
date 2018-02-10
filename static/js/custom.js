fe33df1af09470e91fee4628be99449561a09f38
Simonas Viliunas <vilisimo@gmail.com>
2018-02-10 12:33:05 +0200

    Unify project list behavior
    Previously, hovering on <a> elements did not make them bold.
    No more.

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,5 +1,5 @@
-$('.project-title').click(function() {
-    $(this).parent().toggleClass('active');
+$('.project > li').click(function() {
+    $(this).toggleClass('active');
     const project = $(this).closest('.project');
     project.children('.project-content').toggle(300).removeClass('hidden');
     project.siblings().children('.project-content').hide(300).addClass('hidden');
